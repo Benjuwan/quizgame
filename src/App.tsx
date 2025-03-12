bec356eb2967e424c1558bc3c63213510de251bf
benjuwan <benjuwan.banquet12@gmail.com>
2025-03-12 23:16:35 +0900

    fix: 脆弱性の更新とスタイルの修正.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import { useContext } from "react";
 import { QuestionCounterContext } from "./providers/QuestionCounterContext";
-import { FetchAnswersDataContext } from "./providers/GetFetchAnswersDataContext";
 import { Header } from "./layout/Header";
 import { FirstViewer } from "./FirstViewer";
 import { Footer } from "./layout/Footer";
 
 export const App = () => {
   const { questionCounter } = useContext(QuestionCounterContext);
-  const { fetchAnswersData } = useContext(FetchAnswersDataContext);
 
   return (
-    <div
-      className={`RootWrapper bodyColor_0${questionCounter}`}
-      style={fetchAnswersData.length > 0 ? { 'height': 'auto' } : undefined}
-    >
+    <div className={`RootWrapper bodyColor_0${questionCounter}`}>
       <Header />
       <FirstViewer />
       <Footer />
